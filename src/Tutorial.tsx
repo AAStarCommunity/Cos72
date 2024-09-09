392c2b01b0676de2f555bbdea14e3a4d76f26da9
clarkwu <superwunc@qq.com>
2024-09-09 10:00:34 +0800

    update Tutorial

--- src/Tutorial.tsx
+++ src/Tutorial.tsx
@@ -4,15 +4,23 @@ import { Panel } from "primereact/panel";
 import styles from "./Tutorial.module.css";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CopyBlock, nord } from "react-code-blocks";
 import { toast, ToastContainer } from "react-toastify";
 import { AAStarClient } from "./sdk";
 import { NetworkdConfig, networkIds } from "./config";
 import { ethers } from "ethers";
+import { Stepper } from 'primereact/stepper';
+import { StepperPanel } from 'primereact/stepperpanel';
 import TetherToken from "./contracts/TetherToken.json";
+import { Fieldset } from "primereact/fieldset";
+import { InputTextarea } from "primereact/inputtextarea";
 const TetherTokenABI = TetherToken.abi;
 function App() {
+  const stepperRef = useRef(null);
+  const [bundlerUrl, setBundlerUrl] = useState(undefined);
+  const [paymasterUrl, setPaymasterUrl] = useState(undefined);
+  const [paymasterStrategyCode, setPaymasterStrategyCode] = useState(null);
   const [account, setAccount] = useState(null);
   const [amount, setAmount] = useState(null);
   const [mintLoading, setMintLoading] = useState(false);
@@ -20,17 +28,28 @@ function App() {
     const id = toast.loading("Please wait...");
     setMintLoading(true);
     try {
-
-      // 第一步 创建 AAStarClient
-      const bundlerConfig = NetworkdConfig[networkIds.OP_SEPOLIA].bundler[0];
-
-      const payMasterConfig = NetworkdConfig[networkIds.OP_SEPOLIA].paymaster[0];
-
       const smartAccount = new AAStarClient({
-        bundler: bundlerConfig as any, // bunder 配置
-        paymaster: payMasterConfig as any, // payMaserter 配置
+        aaConfig: {
+          provider: "SimpleAccount"
+        },
+        bundler: {
+          provider: "pimlico",
+          config: {
+            url: bundlerUrl  as any         
+          }
+        },
+        paymaster:  {
+          provider: "aastar",
+          config: {
+            url:  paymasterUrl as any,
+            option: {
+              strategy_code: paymasterStrategyCode as any,
+              version: "v0.6",
+            },
+          },
+        },
 
-        rpc: NetworkdConfig[networkIds.OP_SEPOLIA].rpc, // rpc节点地址,
+        rpc: NetworkdConfig[networkIds.OP_SEPOLIA].rpc,
       });
 
       // 第二步 创建合约调用参数
@@ -86,6 +105,39 @@ function App() {
     <div className={styles.Root}>
       <div className={styles.DemoPanel}>
         <Panel header="Mint Test Token Demo">
+          <Fieldset legend="Config" className={styles.Config}>
+          <div className={styles.inputRow}>
+            <div>Bundler Url</div>
+            <InputTextarea
+              value={bundlerUrl}
+              className={styles.input}
+              onChange={(event) => {
+                setBundlerUrl(event.target.value as any);
+              }}
+            ></InputTextarea>
+          </div>
+          <div className={styles.inputRow}>
+            <div>Paymaster Url</div>
+            <InputTextarea
+              value={paymasterUrl}
+              className={styles.input}
+              onChange={(event) => {
+                setPaymasterUrl(event.target.value as any);
+              }}
+            ></InputTextarea>
+          </div>
+          <div className={styles.inputRow}>
+            <div>Paymaster Strategy Code</div>
+            <InputText
+              value={paymasterStrategyCode}
+              className={styles.input}
+              onChange={(event) => {
+                setPaymasterStrategyCode(event.target.value as any);
+              }}
+            ></InputText>
+          </div>
+          </Fieldset>
+    
           <div className={styles.inputRow}>
             <div>Account</div>
             <InputText
@@ -113,25 +165,49 @@ function App() {
         </Panel>
       </div>
       <div className={styles.SourcePanel}>
-        <Panel header="Source">
-        <CopyBlock
+        <Panel header="Tutorial">
+        <Stepper ref={stepperRef}  orientation="vertical">
+                <StepperPanel header="Apply Bundler">
+                    <div>Apply for API Key at <a href="https://dashboard.pimlico.io/apikeys">https://dashboard.pimlico.io/apikeys</a>. Please select OP Sepolia for the network and fill in the applied RPC URL into the Bundler Url input box on the left.</div>
+                </StepperPanel>
+                <StepperPanel header="Apply Paymaster">
+                  <ol>
+                    <li> <div>Apply for API Key at <a href="https://dashboard.aastar.io/api-keys">https://dashboard.aastar.io/api-keys</a>. Please select OP Sepolia for the network and fill in the applied RPC URL into the Paymaster Url input box on the left.</div></li>
+                    <li>      
+                    <div>Create Strategy at <a href="https://dashboard.aastar.io/strategy/create">https://dashboard.aastar.io/strategy/create</a>. Copy the strategy code into the left Strategy code input box.</div></li>
+                  </ol>
+         
+                </StepperPanel>
+                <StepperPanel header="Code">
+                <CopyBlock
             showLineNumbers={true}
             theme={nord}
-            text={`  const mintUSDT = async () => {
+            text={` const mintUSDT = async () => {
     const id = toast.loading("Please wait...");
     setMintLoading(true);
     try {
-
-      // 第一步 创建 AAStarClient
-      const bundlerConfig = NetworkdConfig[networkIds.OP_SEPOLIA].bundler[0];
-
-      const payMasterConfig = NetworkdConfig[networkIds.OP_SEPOLIA].paymaster[0];
-
       const smartAccount = new AAStarClient({
-        bundler: bundlerConfig as any, // bunder 配置
-        paymaster: payMasterConfig as any, // payMaserter 配置
+        aaConfig: {
+          provider: "SimpleAccount"
+        },
+        bundler: {
+          provider: "pimlico",
+          config: {
+            url: bundlerUrl  as any         
+          }
+        },
+        paymaster:  {
+          provider: "aastar",
+          config: {
+            url:  paymasterUrl as any,
+            option: {
+              strategy_code: paymasterStrategyCode as any,
+              version: "v0.6",
+            },
+          },
+        },
 
-        rpc: NetworkdConfig[networkIds.OP_SEPOLIA].rpc, // rpc节点地址,
+        rpc: NetworkdConfig[networkIds.OP_SEPOLIA].rpc,
       });
 
       // 第二步 创建合约调用参数
@@ -151,13 +227,14 @@ function App() {
       console.log("Waiting for transaction...");
       // 第三步 发送 UserOperation
       const response = await smartAccount.sendUserOperation(callTo, callData);
-    
+     
       toast.update(id, {
         render: "Success",
         type: "success",
         isLoading: false,
         autoClose: 5000,
       });
+
     } catch (error) {
       console.log(error);
       toast.update(id, {
@@ -171,6 +248,10 @@ function App() {
   };`}
             language="typescript"
           />
+                
+                </StepperPanel>
+            </Stepper>
+      
         </Panel>
       </div>
       <ToastContainer />
